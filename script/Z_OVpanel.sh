2e5b4660462e89f27ddfe78650b734c2af9907a2
Đỗ Minh Trí <103892814+dominhtri2007@users.noreply.github.com>
2022-12-07 06:58:56 -0800

    Update Z_OVpanel.sh

--- script/Z_OVpanel.sh
+++ script/Z_OVpanel.sh
@@ -7,7 +7,7 @@ chattr +i /www/server/panel/data/plugin.json
 chattr -i /www/server/panel/data/repair.json
 rm /www/server/panel/data/repair.json
 cd /www/server/panel/data
-wget https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/resource/repair.json
+wget https://raw.githubusercontent.com/dominhtri2007/CrackAapanel/main/resource/repair.json
 chattr +i /www/server/panel/data/repair.json
 rm /www/server/panel/data/plugin.json
 cd /www/server/panel/data
@@ -15,11 +15,11 @@ wget https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/resource/plugin.json
 chattr +i /www/server/panel/data/plugin.json
 rm /www/server/panel/BTPanel/static/js/soft.js
 cd /www/server/panel/BTPanel/static/js
-wget https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/resource/soft.js
+wget https://raw.githubusercontent.com/dominhtri2007/CrackAapanel/main/resource/soft.js
 chattr +i /www/server/panel/data/soft.js
 rm /www/server/panel/BTPanel/static/js/index.js
 cd /www/server/panel/BTPanel/static/js
-wget https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/resource/index.js
+wget https://raw.githubusercontent.com/dominhtri2007/CrackAapanel/main/resource/index.js
 chattr +i /www/server/panel/data/index.js
 red "cracked đã hoàn  tất"
 
