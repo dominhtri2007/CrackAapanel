d0429889a416f9c9c9f5626ec1ef349707dee02d
Đỗ Minh Trí <103892814+dominhtri2007@users.noreply.github.com>
2022-12-11 20:43:11 +0700

    Update

--- script/Z_OVpanel.sh
+++ script/Z_OVpanel.sh
@@ -1,6 +1,6 @@
 
 # đừng copy
-red "xin chờ 3s sẽ cờ rách xong "
+red "xin chờ 3s sẽ crack xong "
 sed -i 's|"endtime": -1|"endtime": 999999999999|g' /www/server/panel/data/plugin.json
 sed -i 's|"pro": -1|"pro": 0|g' /www/server/panel/data/plugin.json
 chattr +i /www/server/panel/data/plugin.json
@@ -11,7 +11,7 @@ wget https://raw.githubusercontent.com/dominhtri2007/CrackAapanel/main/resource/
 chattr +i /www/server/panel/data/repair.json
 rm /www/server/panel/data/plugin.json
 cd /www/server/panel/data
-wget https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/resource/plugin.json
+wget https://raw.githubusercontent.com/dominhtri2007/CrackAapanel/main/resource/plugin.json
 chattr +i /www/server/panel/data/plugin.json
 rm /www/server/panel/BTPanel/static/js/soft.js
 cd /www/server/panel/BTPanel/static/js
